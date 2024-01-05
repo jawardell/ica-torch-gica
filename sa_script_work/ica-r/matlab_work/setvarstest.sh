ffe58aaef7047295625b8b7ca7eaf9198f7573c1
Joanne Wardell <joanne.wardell6@gmail.com>
2024-01-05 23:35:30 +0000

    Major debugging session for gigicar.py, runs but SMs look odd.

--- sa_script_work/ica-r/matlab_work/setvarstest.sh
+++ sa_script_work/ica-r/matlab_work/setvarstest.sh
@@ -5,7 +5,7 @@ module load matlab
 
 export SLURM_ARRAY_TASK_ID=0
 
-export PATHS_FILE=/data/users2/jwardell1/ica-torch-gica/standalone_gica_script/ica-r/matlab_work/paths
+export PATHS_FILE=/data/users2/jwardell1/ica-torch-gica/sa_script_work/ica-r/matlab_work/datasets/fbirn-project/paths_fbirn
 
 
 
@@ -18,14 +18,15 @@ export sub_ix=$(( 5*$SLURM_ARRAY_TASK_ID + 3 ))
 export out_ix=$(( 5*$SLURM_ARRAY_TASK_ID + 4 ))
 
 
-export FMRI_NIFTI=${paths_array[${func_ix}]}
+#export FMRI_NIFTI=${paths_array[${func_ix}]}
+export FMRI_NIFTI=/data/users2/jwardell1/ica-torch-gica/sa_script_work/gica/group_level_analysis/Neuromark_fMRI_1.0.nii
 export SM_NIFTI=${paths_array[${sm_ix}]}
 export MASK_NIFTI=${paths_array[${mask_ix}]}
 export SUBID=${paths_array[${sub_ix}]}
 export OUTPUT_DIR=${paths_array[${out_ix}]}
 
 
-export SCRIPT=/data/users2/jwardell1/ica-torch-gica/standalone_gica_script/ica-r/matlab_work/gigicar.m
+export SCRIPT=/data/users2/jwardell1/ica-torch-gica/sa_script_work/ica-r/matlab_work/gigicar.m
 
 #gunzip ${FMRI_NIFTI}.gz
 #matlab -batch "setenv('inputArg1', '${FMRI_NIFTI}'); setenv('inputArg2', '${SM_NIFTI}'); setenv('inputArg3', '${SUBID}'); setenv('inputArg4', '${MASK_NIFTI}'); setenv('inputArg5', '${OUTPUT_DIR}'); run('${SCRIPT}')"
