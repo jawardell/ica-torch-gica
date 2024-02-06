469f5b860bb97a30f14f5edfa936eb986d520130
Joanne Wardell <joanne.wardell6@gmail.com>
2024-02-06 00:19:26 +0000

    Add pytorch work

--- sa_script_work/ica-r/matlab_work/datasets/oulu-project/submit.sh
+++ sa_script_work/ica-r/matlab_work/datasets/oulu-project/submit.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 project_dir=/data/users2/jwardell1/ica-torch-gica/sa_script_work/ica-r/matlab_work
-paths_file=${project_dir}/datasets/DATASETNAME-project/paths_DATASETNAME
+paths_file=${project_dir}/datasets/oulu-project/paths_oulu
 
 num_lines=`wc -l <  $paths_file`
 num_total_runs=$(( $num_lines / 5 ))
 
-runix=$(( $num_total_runs - 1 ))
+startix=0
+#endix=$(( $num_total_runs - 1 ))
+endix=10
 
-sbatch --array=0-${runix} ${project_dir}/gigicar.job
+sbatch --array=${startix}-${endix} ${project_dir}/datasets/oulu-project/gigicar.job
 #sbatch --array=0-${runix}%10 ${project_dir}/gigicar.job
--- sa_script_work/ica-r/python_work/gigicar.py
+++ sa_script_work/ica-r/python_work/gigicar.py
@@ -1,6 +1,4 @@
-import torch
-import torch.nn.functional as F
-from torch.linalg import norm
+import numpy as np
 import sys
 import os
 import nibabel as nib
@@ -9,120 +7,141 @@ import datetime
 from scipy.linalg import sqrtm
 
 
-def gigicar(FmriMatr, ICRefMax):
-    # Convert numpy arrays to PyTorch tensors
-    FmriMatr = torch.tensor(FmriMatr, dtype=torch.float32)
-    ICRefMax = torch.tensor(ICRefMax, dtype=torch.float32)
+def gigicar(FmriMatr,ICRefMax):
+	#written by Brad Baker, MRN. 2018.
+	#Based on MATLAB code by 
 
-    # Extract dimensions
-    n, m = FmriMatr.shape
-    n2, m2 = ICRefMax.shape
+	#Input:
+	#FmriMatr is the observed data with size of timepoints*voxelvolums (numpy)
+	#ICRefMax includes the reference signals (numpy)
 
-    # Subtract mean from observed data
-    FmriMat = FmriMatr - FmriMatr.mean(dim=1, keepdim=True)
+	#Output
+	#ICOutMax includes the estimated ICs
+	#TCMax is the obtained mixing matrix
 
-    # Calculate covariance matrix
-    CovFmri = FmriMat @ FmriMat.t() / m
+	n,m = FmriMatr.shape
+	n2,m2 = ICRefMax.shape
+	logger.info("\n\nStarting GIGICA at %s" % str(datetime.datetime.now()))
+	logger.info("Reference size %s\tSignal size %s" % (FmriMatr.shape, ICRefMax.shape) )
+	FmriMat=FmriMatr - np.tile(np.mean(FmriMatr,1),(m,1)).T
+	CovFmri = np.matmul(FmriMat, FmriMat.T) / m
+	logger.info("Performing PCA reduction on signal")
+	[D,E]=np.linalg.eig(CovFmri)
+	EsICnum=ICRefMax.shape[0] #EsICnum can be a number that is less than size(ICRefMax,1)
+	index = np.argsort(D)
+	eigenvalues = D[index]
+	cols=E.shape[1]
+	Esort=np.zeros(E.shape)
+	dsort=np.zeros(eigenvalues.shape)
+	for i in range(cols):
+	    Esort[:,i] = E[:,index[cols-i-1] ]
+	    dsort[i]   = D[index[cols-i-1] ]
 
-    # Perform PCA reduction on signal
-    D, E = torch.linalg.eig(CovFmri)
-    D = D[:, 0].real if len(D.shape) > 1 else D.real  # Extract real parts of eigenvalues
-    EsICnum = ICRefMax.shape[0]
+	thr=0 #you can change the parameter. such as thr=0.02
+	numpc=0
+	for i in range(cols):
+	    if dsort[i]>thr:
+	        numpc=numpc+1
+	logger.info("Performing PCA for %d components" % numpc)
 
-    # Sort eigenvalues and eigenvectors
-    eigenvalues, index = D[:, 0].sort(descending=True)
-    Esort = E[:, index]
-    dsort = eigenvalues
+	Epart=Esort[:,:numpc].real
+	dpart=dsort[:numpc].real
+	Lambda_part=np.diag(dpart).real
+	logger.info("Whitening source signal")
+	WhitenMatrix=np.matmul((np.linalg.inv(sqrtm(Lambda_part))), Epart.T)
+	logger.info("Done whitening")
+	Y=np.matmul(WhitenMatrix, FmriMat)
+	logger.info("Done projecting")
+	logger.info("Normalizing...")
+	if thr<1e-10 and numpc<n:
+		for i in range(Y.shape[0]):
+			Y[i,:]=Y[i,:]/np.std(Y[i,:])
 
-    thr = 0  # Set your threshold value here
-    numpc = (dsort > thr).sum()
+	logger.info("Normalizing source signal")
+	Yinv=np.linalg.pinv(Y)
+	ICRefMaxN=np.zeros((EsICnum,m2))
+	ICRefMaxC=ICRefMax - np.tile(np.mean(ICRefMax,1), (m2, 1)).T
+	for i in range(EsICnum):
+	    ICRefMaxN[i,:]=ICRefMaxC[i,:]/np.std(ICRefMaxC[i,:])
+	
+	logger.info("Computing negentropy")
+	NegeEva=np.zeros((EsICnum,1))
+	for i in range(EsICnum):
+	    NegeEva[i]=nege(ICRefMaxN[i,:])
 
-    # Perform PCA for selected components
-    Epart = Esort[:, :numpc]
-    dpart = dsort[:numpc]
-    Lambda_part = torch.diag(dpart)
+	iternum=10
+	a=0.5
+	b=1-a
+	EGv=0.3745672075
+	ErChuPai=2/np.pi
+	ICOutMax=np.zeros((EsICnum,m))
+	logger.info("Starting with EGv=%f" % EGv)
+	for ICnum in range(EsICnum):
+		logger.info('gigicar component: %d/%d' % (ICnum, EsICnum))
+		reference=ICRefMaxN[ICnum,:]
+		wc=(np.matmul(reference, Yinv)).T
+		wc=wc/np.linalg.norm(wc)
+		y1=np.matmul(wc.T, Y)
+		EyrInitial=(1/m)*(y1)@reference.T
+		NegeInitial=nege(y1)
+		c=(np.tan((EyrInitial*np.pi)/2))/NegeInitial
+		IniObjValue=a*ErChuPai*np.arctan(c*NegeInitial)+b*EyrInitial
 
-    # Whitening source signal
-    WhitenMatrix = Epart.t() @ torch.linalg.inv(sqrtm(Lambda_part))
-    Y = WhitenMatrix @ FmriMat
-
-    # Normalize source signal
-    Y = F.normalize(Y, dim=1)
-
-    # Normalize reference signals
-    ICRefMaxN = (ICRefMax - ICRefMax.mean(dim=1, keepdim=True)) / ICRefMax.std(dim=1, keepdim=True)
-
-    # Computing negentropy
-    NegeEva = torch.zeros((EsICnum, 1))
-    for i in range(EsICnum):
-        NegeEva[i] = nege(ICRefMaxN[i, :])
-
-    iternum = 10
-    a = 0.5
-    b = 1 - a
-    EGv = 0.3745672075
-    ErChuPai = 2 / 3.141592653589793
-
-    ICOutMax = torch.zeros((EsICnum, m))
-    logger.info("Starting with EGv=%f" % EGv)
-    for ICnum in range(EsICnum):
-        logger.info('gigicar component: %d/%d' % (ICnum, EsICnum))
-        reference = ICRefMaxN[ICnum, :]
-        wc = (reference @ torch.linalg.pinv(Y)).t()
-        wc = wc / norm(wc)
-        y1 = wc.t() @ Y
-        EyrInitial = (1 / m) * (y1) @ reference.t()
-        NegeInitial = nege(y1)
-        c = (torch.tan((EyrInitial * 3.141592653589793) / 2)) / NegeInitial
-        IniObjValue = a * ErChuPai * torch.arctan(c * NegeInitial) + b * EyrInitial
-
-        itertime = 1
-        Nemda = 1
-        for i in range(iternum):
-            Cosy1 = torch.cosh(y1)
-            logCosy1 = torch.log(Cosy1)
-            EGy1 = logCosy1.mean()
-            Negama = EGy1 - EGv
-            dim = y1.shape[0] if len(y1.shape) > 1 else y1.shape[0]
-            EYgy = (1 / m) * Y @ (torch.tanh(y1).reshape(1, dim)).t()
-            Jy1 = (EGy1 - EGv)**2
-            KwDaoshu = ErChuPai * c * (1 / (1 + (c * Jy1)**2))
-            Simgrad = (1 / m) * Y @ reference.t()
-            g = a * KwDaoshu * 2 * Negama * EYgy + b * Simgrad.view(Simgrad.shape[0], 1)
-            g_norm = torch.linalg.norm(g)
-            d = g / g_norm
-            wx = wc.view(wc.shape[0], 1) + Nemda * d
-            wx = wx / norm(wx)
-            y3 = wx.t() @ Y
-            PreObjValue = a * ErChuPai * torch.arctan(c * nege(y3)) + b * (1 / m) * y3 @ reference.t()
-            ObjValueChange = PreObjValue - IniObjValue
-            ftol = 0.02
-            dg = g.t() @ d
-            ArmiCondiThr = Nemda * ftol * dg
-            if ObjValueChange < ArmiCondiThr:
-                Nemda = Nemda / 2
-                continue
-            if torch.allclose(wc.t() @ wx, torch.zeros(1), atol=1e-5):
-                break
-            elif itertime == iternum:
-                break
-            IniObjValue = PreObjValue
-            y1 = y3
-            wc = wx
-            itertime = itertime + 1
-
-        Source = wx.t() @ Y
-        ICOutMax[ICnum, :] = Source
-
-    TCMax = (1 / m) * FmriMatr @ ICOutMax.t()
-    return ICOutMax, TCMax
-
+		itertime=1
+		Nemda=1
+		for i in range(iternum):
+			Cosy1=np.cosh(y1)
+			logCosy1=np.log(Cosy1)
+			EGy1=np.mean(logCosy1)
+			Negama=EGy1-EGv
+			if len(y1.shape) > 1 :
+				if y1.shape[0] > y1.shape[1]:
+					dim = y1.shape[0]
+				else:
+					dim = y1.shape[1]
+			else:
+				dim = y1.shape[0]
+			EYgy=(1/m)*Y@(np.tanh(y1).reshape(1,dim)).T
+			Jy1=(EGy1-EGv)**2
+			KwDaoshu=ErChuPai*c*(1/(1+(c*Jy1)**2))
+			Simgrad=(1/m)*Y@reference.T
+			g=a*KwDaoshu*2*Negama*EYgy+b*Simgrad.reshape(Simgrad.shape[0],1)
+			logging.info(g.shape)
+			gtg = np.matmul(g.T, g)
+			d=g/((gtg)**0.5)
+			#d=g@(np.linalg.inv(gtg**0.5))
+			wx=wc.reshape(wc.shape[0],1)+Nemda*d
+			wx=wx/np.linalg.norm(wx)
+			y3=wx.T.dot(Y)
+			PreObjValue=a*ErChuPai*np.arctan(c*nege(y3))+b*(1/m)*y3@reference.T
+			ObjValueChange=PreObjValue-IniObjValue
+			ftol=0.02
+			dg=g.T@d
+			ArmiCondiThr=Nemda*ftol*dg
+			if ObjValueChange<ArmiCondiThr:
+				Nemda=Nemda/2
+				continue
+			if np.all((wc - wx).T @ (wc - wx) < 1.e-5):
+				break
+			elif itertime==iternum:
+				break
+			IniObjValue=PreObjValue
+			y1=y3
+			wc=wx
+			itertime=itertime+1
+		Source=wx.T@Y
+		ICOutMax[ICnum,:]=Source
+	TCMax=(1/m)*FmriMatr@ICOutMax.T
+	return ICOutMax,TCMax
 
 def nege(x):
-    y = torch.log(torch.cosh(x))
-    E1 = y.mean()
-    E2 = 0.3745672075
-    return (E1 - E2)**2
+	y=np.log(np.cosh(x))
+	E1=np.mean(y)
+	E2=0.3745672075
+	return (E1- E2)**2
+
+def mask_img(img, mask):
+	return img[mask==1,:]
 
 
 ########################################################################
@@ -205,52 +224,77 @@ if not os.path.isfile(template_file):
 
 '''
 
-
-
-
-# Load images
+import scipy.io as sio
 src_img = nib.load(func_file)
-src_data = torch.tensor(src_img.get_fdata(), dtype=torch.float32)
+src_data = src_img.get_fdata()
+#src_data = np.load('/data/users2/jwardell1/python_debug/sub_mat.npy')
+#src_data_2 = sio.loadmat('/data/users2/jwardell1/python_debug/sub_mat.mat')['sub_mat']
+#print(f'src_data - src_data_2 {src_data - src_data_2}')
 
 ref_img = nib.load(template_file)
-ref_data = torch.tensor(ref_img.get_fdata(), dtype=torch.float32)
+ref_data = ref_img.get_fdata()
+#ref_data = '/data/users2/jwardell1/python_debug/group_mat.npy'
+#ref_data = np.load('/data/users2/jwardell1/python_debug/group_mat.npy')
+#ref_data_2 = sio.loadmat('/data/users2/jwardell1/python_debug/group_mat.mat')['group_mat']
+#print(f'ref_data - ref_data_2 {ref_data - ref_data_2}')
 
 mask_img = nib.load(mask_file)
-mask_data = torch.tensor(mask_img.get_fdata(), dtype=torch.float32)
+mask_data = mask_img.get_fdata()
+idx = np.where(mask_img.dataobj)
 
-# Create idx tensor
-idx = torch.nonzero(mask_data).t()
-
-# Mask source and reference images
-src_data = src_data[idx[0], idx[1], idx[2], :].t()
+#mask source and reference images
+src_data = src_data[*idx,:]
 print(f'src_data.shape {src_data.shape}')
 
-ref_data = ref_data[idx[0], idx[1], idx[2], :].t()
+ref_data = ref_data[*idx,:]
 print(f'ref_data.shape {ref_data.shape}')
 
-# Convert idx to numpy for compatibility with existing code
-idx_np = idx.cpu().numpy()
+ICOutMax, TCMax = gigicar(src_data.T, ref_data.T)
 
-# Continue with the rest of your PyTorch code...
-ICOutMax, TCMax = gigicar(src_data, ref_data)
+# save time courses file
+tcfilename = '{}/{}_TCMax.npy'.format(output_dir, sub_id)
+np.save(tcfilename,TCMax)
 
-# Save time courses file
-tcfilename = f'{output_dir}/{sub_id}_TCMax_PyTorch.npy'
-np.save(tcfilename, TCMax)
 
-# Reconstruct brain voxels
+# reconstruct brain voxels
 xdim, ydim, zdim = mask_data.shape
 n_comp = ICOutMax.shape[0]
-image_stack = torch.zeros((xdim, ydim, zdim, n_comp))
+image_stack = np.zeros((xdim, ydim, zdim, n_comp))
+image_stack[*idx,:] = ICOutMax.T
 
-# Convert idx to numpy for compatibility with existing code
-idx_np = idx.cpu().numpy()
-
-image_stack[idx_np[0], idx_np[1], idx_np[2], :] = ICOutMax.t()
-
-# Save as nifti
-nifti_img = nib.Nifti1Image(image_stack.numpy(), affine=mask_img.get_qform())
+# save as nifti
+nifti_img = nib.Nifti1Image(image_stack, affine=mask_img.get_qform())
 nifti_img.header.set_sform(mask_img.header.get_sform(), code=mask_img.get_qform('code')[1])
 nifti_img.header.set_qform(mask_img.header.get_qform(), code=mask_img.get_qform('code')[1])
-nifti_file = f'{output_dir}/{sub_id}_ICOutMax_PyTorch.nii.gz'
+nifti_file = '{}/{}_ICOutMax_SANITYCHECK_PYTHON.nii.gz'.format(output_dir, sub_id)
 nib.save(nifti_img, nifti_file)
+
+
+'''
+import scipy.io as sio
+
+
+
+
+mask = sio.loadmat('mask.mat')['mask'].flatten()
+ref_img = nib.load(template_file)
+ref_img = np.array(ref_img.dataobj)
+print(f'ref_img.shape {ref_img.shape}')
+ref_img = ref_img.reshape(np.prod(ref_img.shape[0:3]), ref_img.shape[3])
+
+
+print(f'template_file {template_file}')
+ref_img = mask_img(ref_img, mask)
+src_img = nib.load(func_file)
+
+
+print(f'func_file {func_file}')
+src_img = np.array(src_img.dataobj)
+src_img = src_img.reshape(np.prod(src_img.shape[0:3]), src_img.shape[3])
+print(f'src_img.shape {src_img.shape}')
+
+src_img = mask_img(src_img, mask)
+print(f'src_img.shape {src_img.shape}')
+print(f'ref_img.shape {ref_img.shape}')
+ICOutMax, TCMax = gigicar(src_img.T, ref_img.T)
+'''
